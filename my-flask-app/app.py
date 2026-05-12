83564b46dbfcc0f33cb7681fd387cc6a818607ab
unknown <02250353.cst@rub.edu.bt>
2026-05-11 19:19:37 -0700

    Finalizd A4

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps!"
+    return "Hello DevOps! It was a successful deployment using render."
 
 if __name__ == '__main__':
     app.run()
